988e2691ed2c1f15180eeed024187931e2a47d95
test <brucemclarenp4@gmail.com>
2025-05-08 23:54:52 +0900

    楽曲ページ実装

--- songs.php
+++ songs.php
@@ -8,16 +8,43 @@
   try {
     require('connect.php');
     $dbh->query('SET NAMES utf8');
+    $sql='SELECT id, title, photo FROM songs ';
+    $stmt=$dbh->prepare($sql);
+    $stmt->execute();
 
     $dbh = null;
+
+    while(true)
+    {
+      $rec=$stmt->fetch(PDO::FETCH_ASSOC);
+      if(!$rec)
+      {
+        break;
+      }
+
+      if($rec['photo'] === '')
+      {
+        $img_name = '';
+      }
+      else
+      {
+        $img_name = '<img style="width:360px" src="photos/'.$rec['photo'].'">';
+      }
+
+      echo '<span class="img_style">'.
+          '<a href="songdetail.php?id='.$rec['id'].'">'.
+          $img_name.
+          '<br />'.
+          $rec['title'].
+            '</a>'.
+          '<br />'.
+          '</span>';
+    }
   } catch (Exception $e) {
     echo 'ただいま障害により大変ご迷惑をお掛けしております。';
     exit();
   }
   ?>
-  ここに楽曲一覧
 </main>
 
 <?php include('partial/footer.php') ?>
-</body>
-</html>

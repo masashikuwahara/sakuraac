dcbb2517a8e96f89ea8f7ceea6743de53f4e7c61
test <brucemclarenp4@gmail.com>
2025-05-20 00:26:00 +0900

    不要ファイル削除

--- admin/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dsn = 'mysql:dbname=sakuraac;host=localhost;charset=utf8';
-$user = 'root';
-$password = '';
-$dbh = new PDO($dsn, $user, $password);
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-?>

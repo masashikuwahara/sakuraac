e0fb6808145aa8d170b79a6f4475edd690fcaa21
test <brucemclarenp4@gmail.com>
2025-06-30 22:52:20 +0900

    楽曲検索機能実装

---
+++ admin/songs/songs_search.php
@@ -0,0 +1,71 @@
+<?php
+  require_once('../library.php');
+  session();
+  $my_sea=htmlspecialchars($_GET["s"], ENT_QUOTES);
+  $s = mb_convert_encoding($my_sea, "UTF-8", "auto");
+  try{
+      require('../../connect.php');
+  } catch(PDOException $e){
+      echo "失敗:" . $e->getMessage() . "\n";
+      exit();
+  }
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style.css">
+    <title><?php echo $s ?>の検索結果</title>
+</head>
+<style>
+  .result {
+    color: red;
+  }
+</style>
+<body>
+<header>
+  <h1>検索結果</h1>
+</header>
+  <main>
+    <?php
+    echo '<form method="post" action="members_branch.php">';
+    if($s != ''){
+      echo "<div class='s'> 「{$s}」の検索結果</div>";
+    }
+
+    if($_GET["s"] != ''){
+        $stmt=$dbh->prepare("SELECT * FROM members WHERE name like '%$my_sea%'");
+        $stmt->execute();
+        $t = $stmt->rowCount();
+        if($t > 0){
+          echo "<div class='success'>{$t}件見つかりました</div></br>";
+          while ($r = $stmt->fetch()){
+            echo '<input type="radio" name="id" value="'.$r['id'].'">';
+            echo $r['name']. '<br>';
+          } 
+          echo '<br/>';
+          echo '<input class="btn" type="submit" name="disp" value="参照">';
+          echo '<input class="btn" type="submit" name="edit" value="修正">';
+          echo '<input disabled class="btn" type="submit" name="delete" value="削除">';
+        }else{
+          echo '<div class="result">そのキーワードでは見つかりませんでした</div>';
+        }
+    }else{
+      echo '<div class="result">キーワードを入力してください</div>';
+    }
+    echo '</form>';
+    ?>
+
+    <H2>もう一度検索する</H2>
+    <form action="members_search.php" method="get">
+      <input class="sea" type="text" name="s" placeholder="メンバー名">
+      <input class="btn" type="submit" value="検索">
+    </form>
+    
+    <p class="btn-group"><a href="index.php" class="btn">トップメニューへ</a></p>
+  </main>
+  <?php include("../partial/footer.php") ?>
+  </body>
+</html>

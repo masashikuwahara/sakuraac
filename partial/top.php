1b9116ec4a78b54cdebae582e3324a92263d1cd5
test <brucemclarenp4@gmail.com>
2025-07-19 02:29:16 +0900

    ファビコン、メタ情報追加

--- partial/top.php
+++ partial/top.php
@@ -1,7 +1,20 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+    <meta name="keyword" content="櫻坂46,サクラザカ46,さくらざか46,sakuarzaka46,上村莉菜,小池美波,齋藤冬優花,井上梨名,
+    遠藤光莉,大園玲,大沼晶保,幸阪茉里乃,武元唯衣,田村保乃,藤吉夏鈴,増本綺良,松田里奈,森田ひかる,守屋麗奈,山﨑天,石森璃花,遠藤理子,
+    小田倉麗奈,小島凪紗,谷口愛季,中嶋優月,的野美青,向井純葉,村井優,村山美羽,山下瞳月" 
+    />
+    <meta name="description" content="櫻坂46のシンプルなデータベースサイトです。メンバー情報、楽曲情報を載せています。">
+    <meta name="format-detection" content="email=no,telephone=no,address=no">
+    <meta property="og:title" content="SAKURA ACCUMULATION">
+    <meta property="og:description" content="櫻坂46のシンプルなデータベースサイトです。メンバー情報、楽曲情報を載せています。">
+    <meta property="og:image" content="https://daytripper.site/sakuraac/images/logo.png">
+    <meta property="og:url" content="https://daytripper.site/sakuraac/"> 
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="SAKURA ACCUMULATION">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
   <link rel="stylesheet" href="css/style.css">
+  <!-- <link rel="icon" href="favicon.ico" type="image/x-icon"> -->

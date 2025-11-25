2de1eb02198e3b114e1a19cbb0b31ddefc327f97
test <brucemclarenp4@gmail.com>
2025-11-25 23:18:00 +0900

    モバイル時メニュー修正

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,10 +11,10 @@
       class="md:hidden focus:outline-none flex flex-col space-y-1 z-50 relative"
       aria-label="Open menu" aria-expanded="false"
     >
-      <span class="block w-1 h-1 bg-white rounded ml-6"></span>
-      <span class="block w-3 h-1 bg-white rounded ml-4"></span>
-      <span class="block w-5 h-1 bg-white rounded ml-2"></span>
-      <span class="block w-7 h-1 bg-white rounded"></span>
+      <span class="block w-1 h-1 bg-white ml-6"></span>
+      <span class="block w-3 h-1 bg-white ml-4"></span>
+      <span class="block w-5 h-1 bg-white ml-2"></span>
+      <span class="block w-7 h-1 bg-white"></span>
     </button>
 
     <!-- メニュー（モバイル用 全画面） -->
@@ -50,7 +50,7 @@
         >
           <a 
             href="{{ route('members.index') }}" 
-            class="px-4 py-2 bg-blue-100 rounded-xl hover:bg-blue-200 hover:text-blue-700 transition"
+            class="px-4 py-2 bg-blue-100 hover:bg-blue-200 hover:text-blue-700 transition"
           >
             メンバー一覧
           </a>
@@ -64,7 +64,7 @@
         >
           <a 
             href="{{ route('songs.index') }}" 
-            class="px-4 py-2 bg-pink-100 rounded-xl hover:bg-pink-200 hover:text-pink-700 transition"
+            class="px-4 py-2 bg-pink-100 hover:bg-pink-200 hover:text-pink-700 transition"
           >
             楽曲一覧
           </a>
@@ -77,8 +77,8 @@
           x-transition:enter-end="opacity-100 translate-y-0"
         >
           <a 
-            href="{{ route('songs.index') }}" 
-            class="px-4 py-2 bg-pink-100 rounded-xl hover:bg-pink-200 hover:text-pink-700 transition"
+            href="{{ route('data.index') }}" 
+            class="px-4 py-2 bg-pink-100 hover:bg-pink-200 hover:text-pink-700 transition"
           >
             データいろいろ
           </a>

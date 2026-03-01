26a2c183bd6c08eb59f5cf6f7292dffe9240a636
test <brucemclarenp4@gmail.com>
2026-03-02 07:56:06 +0900

    誕生日表示

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,17 +5,26 @@ namespace App\Http\Controllers;
 use App\Models\Member;
 use App\Models\Song;
 use App\Models\Changelog;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $today = Carbon::now('Asia/Tokyo')->startOfDay();
+
         $members = Member::inRandomOrder()->take(8)->get();
 
         $songs = Song::inRandomOrder()->take(8)->get();
 
         $logs = Changelog::ordered()->limit(20)->get();
+        
+        $birthdayMembers = Member::query()
+            ->whereMonth('birth', $today->month)
+            ->whereDay('birth', $today->day)
+            ->orderBy('furigana')
+            ->get();
 
-        return view('index', compact('members','songs' ,'logs'));
+        return view('index', compact('members','songs' ,'logs' ,'birthdayMembers'));
     }
 }
